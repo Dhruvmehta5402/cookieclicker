b4a5d48609ca479cdf9a08207324f479c687a4f2
dhruvm4 <dhruvm4@illinois.edu>
2022-06-07 15:17:49 +0530

    Removed useless code and added comments successfully.

--- cookie.py
+++ cookie.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 
-NUMBER_OF_COOKIES = 1000000
+
 URL = 'https://orteil.dashnet.org/cookieclicker/'
     
 #creating object of webdriver and passing driver location path to it
@@ -13,27 +13,36 @@ driver = webdriver.Remote(service.service_url)
 driver.get(URL)
 driver.implicitly_wait(15)
 time.sleep(2)
+
+# Selecting the language in the landing page
 driver.find_element_by_id("langSelect-EN").click()
 time.sleep(2)
 grandma_counter = 0
 farm_counter = 0
 mine_counter = 0
 factory_counter = 0
-i = 0
+# Starting the Game Loop
 while True:
-    i = i + 1
+
+    # Clicking the Cookie button
     driver.find_element_by_id("bigCookie").click()
+
+    # Getting number of cookies
     num_cookies = int(driver.find_element_by_id("cookies").text.split()[0].replace(',', ''))
-    grandma_price = int(driver.find_element_by_id("productPrice1").text.replace(',', ''))
+    
+    # Getting the prices of the diffeent buildings
     try:
+        grandma_price = int(driver.find_element_by_id("productPrice1").text.replace(',', ''))
         farm_price = int(driver.find_element_by_id("productPrice2").text.replace(",", ""))
         mine_price = int(driver.find_element_by_id("productPrice3").text.replace(",", ""))
         factory_price = int(driver.find_element_by_id("productPrice4").text.replace(",", ""))
     except:
+        grandma_price = 100
         farm_price = 1000
         mine_price = 100000
         factory_price = 1000000
     
+    # Clicking the building based on the strategy and number of cookies
     if num_cookies >= grandma_price and grandma_counter < 10:
         time.sleep(0.5)
         driver.find_element_by_id("product1").click()
@@ -51,6 +60,6 @@ while True:
         driver.find_element_by_id("product4").click()
         factory_counter = factory_counter + 1
 
-
+# Ending the game (this is currently of no use but can employ in the future)
 time.sleep(20)
 driver.quit()
